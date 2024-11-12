7a06f8a5db1167857449fa50f4186ece3074cd3e
Zaqyemirul <zaqyhuawei123@gmail.com>
2024-11-13 01:10:34 +0700

    mata sepet gara gara reja

--- app/src/main/java/com/example/yurshop/OnboardingActivity.kt
+++ app/src/main/java/com/example/yurshop/OnboardingActivity.kt
@@ -1,14 +1,30 @@
 package com.example.yurshop
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+        val tvIntroTitle = findViewById<TextView>(R.id.tvIntroTitle)
+        val text = "Selamat Datang di YurShop"
+        val spannableString = SpannableString(text)
+        val colorGreen = ContextCompat.getColor(this, R.color.green)
+        spannableString.setSpan(
+            ForegroundColorSpan(colorGreen),
+            18,
+            21,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        tvIntroTitle.text = spannableString
 
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         val btnRegister = findViewById<Button>(R.id.btnRegister)
