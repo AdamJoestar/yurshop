a2dec9249d7765eab6a7e4216164f113fd1ce069
Zaqyemirul <zaqyhuawei123@gmail.com>
2024-11-13 12:01:35 +0700

    regis dikit

--- app/src/main/java/com/example/yurshop/RegisterActivity.kt
+++ app/src/main/java/com/example/yurshop/RegisterActivity.kt
@@ -1,7 +1,10 @@
 package com.example.yurshop
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
@@ -9,12 +12,41 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val btnRegister = findViewById<Button>(R.id.btnRegister)
-        btnRegister.setOnClickListener {
-            // Setelah register berhasil, kembali ke login
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+        val etNewPassword = findViewById(R.id.etNewPassword) as EditText
+        val ivTogglePassword = findViewById(R.id.ivTogglePassword) as ImageView
+
+        ivTogglePassword.setOnClickListener {
+            if (etNewPassword.inputType == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                etNewPassword.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                ivTogglePassword.setImageResource(R.drawable.ice_eye_off)
+            } else {
+                etNewPassword.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                ivTogglePassword.setImageResource(R.drawable.ice_eye)
+            }
+            etNewPassword.setSelection(etNewPassword.text.length)
+        }
+
+        val etNewConfirmPassword = findViewById(R.id.etNewConfirmPassword) as EditText
+        val ivToggleConfirmPassword = findViewById(R.id.ivToggleConfirmPassword) as ImageView
+
+        ivToggleConfirmPassword.setOnClickListener {
+            if (etNewConfirmPassword.inputType == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                etNewConfirmPassword.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                ivToggleConfirmPassword.setImageResource(R.drawable.ice_eye_off)
+            } else {
+                etNewConfirmPassword.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                ivToggleConfirmPassword.setImageResource(R.drawable.ice_eye)
+            }
+
+            val btnRegister = findViewById<Button>(R.id.btnRegister)
+            btnRegister.setOnClickListener {
+                // Setelah register berhasil, kembali ke login
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
