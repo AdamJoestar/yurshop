4cc9cd2d291d13404074086b28e48cae0098329a
AdamJoestar <adamschoolauto@gmail.com>
2024-11-15 00:25:12 +0700

    gua benerin bagian home dan splash

--- app/src/main/java/com/example/yurshop/MainActivity.kt
+++ app/src/main/java/com/example/yurshop/MainActivity.kt
@@ -3,14 +3,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,9 +28,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val appNameTextView = findViewById<TextView>(R.id.appNameTextView)
+        val text = "Yurshop"
+        val spannableString = SpannableString(text)
+        val colorGreen = ContextCompat.getColor(this, R.color.green)
+        spannableString.setSpan(
+            ForegroundColorSpan(colorGreen),
+            0,
+            3,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        appNameTextView.text = spannableString
+
+        val bottomNavigationBar = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+        bottomNavigationBar?.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.cart -> {
+                    // Handle cart click
+                    true
+                }
+                R.id.home -> {
+                    // Handle home click
+                    true
+                }
+                R.id.profile -> {
+                    // Handle profile click
+                    true
+                }
+                else -> false
+            }
+        }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+        bottomNavigationView.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+
+        val popularTextView = findViewById<TextView>(R.id.popularTextView)
+        val paketTextView = findViewById<TextView>(R.id.paketTextView)
+
+        popularTextView.setOnClickListener {
+            // Handle Popular click
+            // ...
+        }
+
+        paketTextView.setOnClickListener {
+            // Handle Paket click
+            // ...
+        }
+
         viewPager = findViewById(R.id.viewPager)
 
-        val images = listOf(R.drawable.your_logo, R.drawable.your_logo, R.drawable.your_logo) // Ganti dengan gambar Anda
+        val images = listOf(R.drawable.carousel1) // Ganti dengan gambar Anda
         val adapter = CarouselAdapter(images)
         viewPager.adapter = adapter
 
@@ -34,8 +86,10 @@ class MainActivity : AppCompatActivity() {
             viewPager.currentItem = (currentItem + 1) % adapter.itemCount
         }
 
+
         // Mulai auto scroll
         startAutoScroll()
+
     }
 
     private fun startAutoScroll() {
